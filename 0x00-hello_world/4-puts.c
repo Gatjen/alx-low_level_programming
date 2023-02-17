cfa1581e83b316f1ead231e01d5b3e3b5b835445
gatjen <emmanuelndlv1@gmail.com>
2023-02-17 01:45:41 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - a programe that print a line with puts function
- *
- * return: 0 if success
- */
+ *main - print a line with puts function
+ *Return: 0 (success)
+*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
