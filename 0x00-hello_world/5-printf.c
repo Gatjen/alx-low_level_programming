184345dcf5663693dbaa1eb55824cee2bf7aa7c9
gatjen <emmanuelndlv1@gmail.com>
2023-02-18 05:30:43 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - a program that print a line using a printf funtion
- * return: 0 (success)
- */
-
+ *main - a program that print a line using a printf funtion
+ *Return: 0 (success)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-Return (0);
+	return (0);
 }
--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,7 +19,7 @@ int main(void)
 	/* your code goes there */
 	if (n > 0)
 		prinf("%d is positive\n", n);
-	else if (n == 0)
+	else if (n==0)
 		prinf("%d is zero\n", n);
 	else
 		prinf("%d is negative\n", n);
