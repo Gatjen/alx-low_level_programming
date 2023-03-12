be7714e637d91850ec865b0b949765cdb56f2245
gatjen <emmanuelndlv1@gmail.com>
2023-03-12 14:09:03 -0700

    3-mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,7 +14,6 @@ int _atoi(char *s)
 	len = 0;
 	f = 0;
 	digit = 0;
-	
 	while (s[len] != '\0')
 		len++;
 	while (i < len && f == 0)
@@ -37,4 +36,24 @@ int _atoi(char *s)
 	if (f == 0)
 		return (0);
 	return (n);
+}
+/**
+ *main - multiplies two numbers		
+ *@argc: number of arguments
+ *@argv: array of arguments
+ *Return: 0 (Success), 1 (Error)
+*/
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
+	printf("%d\n", result);
+	return (0);
 }
